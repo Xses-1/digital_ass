d77970b3b2e31d4ddb8bf6227b6d15e935991cce
Wiktor Tomanek <wiktort52@gmail.com>
2025-02-18 13:54:58 +0100

    LOL

---
+++ Ass1/computer.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+#include <computer.h>
+
+class Computer(double, double, double, double, double);
+
+
---
+++ Ass1/computer.h
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+class Computer {
+	Computer(double, double, double, double, double);
+
+	double cockRateGHz;
+	double cpiArith;
+	double cpiLoad;
+	double cpiBranch;
+	double cpiStore;
+};
+
