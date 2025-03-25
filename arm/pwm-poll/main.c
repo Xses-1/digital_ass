c349796c20aa66424fe0a250205530153f569e0c
noname <no@email.com>
2025-03-25 16:21:07 +0100

    We did get progres???

--- arm/pwm-poll/main.c
+++ arm/pwm-poll/main.c
@@ -15,10 +15,13 @@
 	(((*(volatile uint32_t *) addr) & (1 << bit)) != 0)
 
 static void init (void) {
+
+        SETBIT(SYSAHBCLKCTRL, 9);  // provide clock to timer        
+
 	SETBIT(TMR32B0TCR, 0);  // Enable timer 1
 	UNSETBIT(TMR32B0TCR, 1);  // Unpause it
 
-	UNSETBIT(TMR32B0MCR, 0);  // Disable interrupt MR0
+	SETBIT(TMR32B0MCR, 0);  // Enable interrupt MR0
 	SETBIT(TMR32B0MCR, 1);  // Enable reset MR0
 	UNSETBIT(TMR32B0MCR, 2);  // Disable stop MR0
 
@@ -30,30 +33,49 @@ static void init (void) {
 	UNSETBIT(TMR32B0MCR, 7);  // Disable reset MR2
 	UNSETBIT(TMR32B0MCR, 8);  // Disable stop MR2
 
-	UNSETBIT(TMR32B0MCR, 9);  // Disable interrupt MR3
+	SETBIT(TMR32B0MCR, 9);  // Enable interrupt MR3
 	UNSETBIT(TMR32B0MCR, 10);  // Disable reset MR3
 	UNSETBIT(TMR32B0MCR, 11);  // Disable stop MR3
 	
-	SETWORD(TMR32B0PR, 1200); // Set prescale
-	SETWORD(TMR32B0MR0, 80); // Set match register
-	SETWORD(TMR32B0MR3, 100); // Set match register
+	SETWORD(TMR32B0PR, 7200); // Set prescale
+	SETWORD(TMR32B0MR0, 8); // Set match register
+	SETWORD(TMR32B0MR3, 9); // Set match register
+
+	SETWORD(TMR32B0TC, 0);
+	SETWORD(TMR32B0PC, 0);
+
+	SETBIT(LPC_GPIO3_DIR, 0);
 }
 
 static void pin_low(void) {
-	SETBIT(GPIO3DATA, 0);
+	SETBIT(LPC_GPIO3_DATA, 0);
 }
 
 static void pin_high(void) {
-	UNSETBIT(GPIO3DATA, 0);
+	UNSETBIT(LPC_GPIO3_DATA, 0);
+}
+
+static void pin_flip(void) {
+	FLIPBIT(LPC_GPIO3_DATA, 0);
 }
 
 int main() {
 	int prev_state, current_state;
-	prev_state = READBIT(GPIO2DATA, 9);
 
 	init();
-	pin_low();
+	pin_high();
 	while (1) {
+		if (READBIT(TMR32B0IR, 3)) {
+			// MR3 match
+			pin_high();
+			UNSETBIT(TMR32B0IR, 3);
+		}
+		else if (READBIT(TMR32B0IR, 0)) {
+			// MR0 match
+			pin_flip();
+			//pin_low();
+			UNSETBIT(TMR32B0IR, 0);
+		}
 	}
 }
 
