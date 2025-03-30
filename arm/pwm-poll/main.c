94adffe3b30dbf867b9f95f7f4189fd4675dd3ff
Xses <wiktort52@gmail.com>
2025-03-30 18:58:22 +0200

    asdfasdf

--- arm/pwm-poll/main.c
+++ arm/pwm-poll/main.c
@@ -19,22 +19,22 @@ static void init (void) {
         SETBIT(SYSAHBCLKCTRL, 9);  // provide clock to timer        
 
 	SETBIT(TMR32B0TCR, 0);  // Enable timer 1
-	//UNSETBIT(TMR32B0TCR, 1);  // Unpause it
+	UNSETBIT(TMR32B0TCR, 1);  // Unpause it
 
 	SETBIT(TMR32B0MCR, 0);  // Enable interrupt MR0
 	SETBIT(TMR32B0MCR, 1);  // Enable reset MR0
 	UNSETBIT(TMR32B0MCR, 2);  // Disable stop MR0
 
-	//UNSETBIT(TMR32B0MCR, 3);  // Disable interrupt MR1
-	//UNSETBIT(TMR32B0MCR, 4);  // Disable reset MR1
-	//UNSETBIT(TMR32B0MCR, 5);  // Disable stop MR1
+	UNSETBIT(TMR32B0MCR, 3);  // Disable interrupt MR1
+	UNSETBIT(TMR32B0MCR, 4);  // Disable reset MR1
+	UNSETBIT(TMR32B0MCR, 5);  // Disable stop MR1
 
-	//UNSETBIT(TMR32B0MCR, 6);  // Disable interrupt MR2
-	//UNSETBIT(TMR32B0MCR, 7);  // Disable reset MR2
-	//UNSETBIT(TMR32B0MCR, 8);  // Disable stop MR2
+	UNSETBIT(TMR32B0MCR, 6);  // Disable interrupt MR2
+	UNSETBIT(TMR32B0MCR, 7);  // Disable reset MR2
+	UNSETBIT(TMR32B0MCR, 8);  // Disable stop MR2
 
-	UNSETBIT(TMR32B0MCR, 9);  // Enable interrupt MR3
-	SETBIT(TMR32B0MCR, 10);  // Disable reset MR3
+	SETBIT(TMR32B0MCR, 9);  // Enable interrupt MR3
+	UNSETBIT(TMR32B0MCR, 10);  // Disable reset MR3
 	UNSETBIT(TMR32B0MCR, 11);  // Disable stop MR3
 	
 	SETWORD(TMR32B0PR, 7200); // Set prescale
@@ -44,19 +44,16 @@ static void init (void) {
 	SETWORD(TMR32B0TC, 0);
 	SETWORD(TMR32B0PC, 0);
 
-	SETBIT(LPC_GPIO1_DIR, 0);
+
+	SETBIT(LPC_GPIO3_DIR, 0);
 }
 
 static void pin_low(void) {
-	SETBIT(LPC_GPIO1_DATA, 0);
+	SETBIT(LPC_GPIO3_DATA, 0);
 }
 
 static void pin_high(void) {
-	UNSETBIT(LPC_GPIO1_DATA, 0);
-}
-
-static void pin_flip(void) {
-	FLIPBIT(LPC_GPIO1_DATA, 0);
+	UNSETBIT(LPC_GPIO3_DATA, 0);
 }
 
 int main() {
@@ -72,11 +69,12 @@ int main() {
 		}
 		else if (READBIT(TMR32B0IR, 0)) {
 			// MR0 match
-			pin_flip();
-			//pin_low();
+			pin_low();
 			UNSETBIT(TMR32B0IR, 0);
 		}
 	}
+
+	return 0; // If the radiation causes the loop to exit
 }
 
 void _start() {
